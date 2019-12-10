1b4d98606c7ea73abdbecc99406ea0b39d1d8773
Roscoe <roscoe@winterwell.com>
2019-12-10 17:49:50 +0000

    MediaCacheServlet in more or less final form, pending testing

--- src/com/media/MediaCacheServlet.java
+++ src/com/media/MediaCacheServlet.java
@@ -2,21 +2,35 @@ package com.media;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 
 import com.winterwell.utils.FailureException;
-import com.winterwell.utils.StrUtils;
 import com.winterwell.utils.io.FileUtils;
 import com.winterwell.utils.web.WebUtils2;
 import com.winterwell.web.FakeBrowser;
-import com.winterwell.web.app.AppUtils;
 import com.winterwell.web.app.IServlet;
-import com.winterwell.web.app.KServerType;
 import com.winterwell.web.app.WebRequest;
 
-
+/**
+ * Hit /mediacache/[complete URL encoded as base64url + filetype] and if the request
+ * doesn't match a previously-cached file, this servlet will catch your request,
+ * retrieve the file, and cache it.
+ * See counterpart code in adserver/adunit/src/util/utils.js --> function wrapUrl
+ * TODO Implement better security - eg sign with salted hash of a valid ad ID.
+ * @author roscoe
+ *
+ */
 public class MediaCacheServlet implements IServlet {
+	// Let's not cache EVERYTHING any rando throws at us.
+	private List<String> acceptExtensions = Arrays.asList(".png", ".jpg", ".jpeg", ".gif", ".svg", ".mp4", ".mpeg4", ".m4v", ".webm");
+	
+	// Don't cache more than 10mb? TODO Think about this for video purposes...
+	private long maxSize = 10000000L;
+	
 	private File webRoot = new File("web/mediacache");
 	{
 		if (!webRoot.exists()) {
@@ -25,31 +39,49 @@ public class MediaCacheServlet implements IServlet {
 		}
 	}
 	
-	private Base64.Encoder encoder = Base64.getEncoder();
+	private Base64.Decoder decoder = Base64.getUrlDecoder();
 	
 	@Override
 	public void process(WebRequest state) throws IOException {
+		// Only the adunit (or someone who has taken a few minutes to figure out the cache system) is allowed to cache new files
+		if (!"good-loop-ad-unit".equals(state.get("from"))) {
+			WebUtils2.sendError(403, "You're not allowed to use this service", state.getResponse());
+			return;
+		}
 		
-		URL origUrl = new URL(state.get("orig"));
-		String origPath = origUrl.getPath();
-		// Preserve .png etc - MIME type of served file appears to depend on this?
-		String extension = origPath.substring(origPath.lastIndexOf('.'));
+		// Strip off leading path components to get filename (we'll be storing to this)
+		String filename = new URL(state.getRequestUrl()).getPath().replaceAll("^.*\\/", "");
 		
+		// Is someone trying to cache something they shouldn't?
+		String extension = filename.substring(filename.lastIndexOf('.'));
+		if (!acceptExtensions.contains(extension)) {
+			WebUtils2.sendError(403, "We don't cache this file type: " + extension, state.getResponse());
+			return;
+		}
+		
+		// Strip off trailing extension to get the encoded URL and decode it
+		String origUrlEncoded = filename.replaceAll("\\..*$", "");
+		String origUrl = new String(decoder.decode(origUrlEncoded));
+		
+		URL fart = new URL(origUrl);
+		HttpURLConnection conn = (HttpURLConnection) fart.openConnection();
+		long size = Integer.parseInt(conn.getHeaderField("Content-Length"));
+		if (size > maxSize) {
+			WebUtils2.sendError(403, "File too big to cache: " + (size / 1024) + "KB", state.getResponse());
+		}
+		conn.disconnect();
+				
+		// Fetch the file. Could reuse the connection we used to check size but this skips a LOT of boilerplate
 		FakeBrowser fb = new FakeBrowser();
-		File targetFile = fb.getFile(origUrl.toString());
-				
-		// File name will be base64-encoded original URL (for filesystem safety) + original extension
-		String encName = new String(encoder.encode(origUrl.toString().getBytes())) + "." + extension;
+		File tmpFile = fb.getFile(origUrl);
 		
-		File destFile = FileUtils.getNewFile(new File(webRoot.getAbsolutePath(), encName));
-		FileUtils.move(targetFile, destFile);
+		// Move the file to the location it was originally requested from, so future calls will be a file hit
+		File destFile = FileUtils.getNewFile(new File(webRoot.getAbsolutePath(), filename));
+		FileUtils.move(tmpFile, destFile);
 		
-		KServerType serverType = AppUtils.getServerType(state);
-		String baseUrl = AppUtils.getServerUrl(serverType, "media.good-loop.com").toString();
-		
-		String destUrl = baseUrl + "/mediacache/" + destFile.getName();
-				
-		state.setRedirect(destUrl);
+		// Redirect the caller to the same place they originally tried - which will now be a file hit
+		// Modifying the URL with a trailing ? because browsers FOR SOME WEIRD REASON don't like a no-op redirect
+		state.setRedirect(state.getRequestUrl() + "?");
 		state.sendRedirect();
 	}
 }
