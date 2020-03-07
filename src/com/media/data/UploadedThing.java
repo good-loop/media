6d66f2e9b0caedddd0047ee4d989cfac38c8a375
Daniel Winterstein <daniel@sodash.com>
2020-03-07 12:37:02 +0000

    pluggable PropControl

---
+++ src/com/media/data/UploadedThing.java
@@ -0,0 +1,13 @@
+package com.media.data;
+
+import com.winterwell.data.AThing;
+
+/**
+ * Status: NOT used yet -- lets see if we can use g-drive or dropbox instead
+ * For storing metadata on misc uploads Based on https://schema.org/Thing
+ * @author daniel
+ *
+ */
+public class UploadedThing extends AThing {
+
+}
