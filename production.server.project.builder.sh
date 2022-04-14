b1c4919ef2cbf45e9d84675826794855fa951174
Aidan <aidan@good-loop.com>
2022-04-14 16:30:41 +0100

    production builder - remove baker SSH invocation & hardcoded branch name

--- production.server.project.builder.sh
+++ production.server.project.builder.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-ssh winterwell@baker.good-loop.com bash <<EOF 
 /home/winterwell/config/build-scripts/builder.sh \
 BUILD_TYPE="production" \
 PROJECT_NAME="media" \
-BRANCH_NAME="master" \
 NAME_OF_SERVICE="mediaserver" \
 GIT_REPO_URL="github.com:good-loop/media" \
 PROJECT_ROOT_ON_SERVER="/home/winterwell/media" \
@@ -12,4 +10,3 @@ PROJECT_USES_NPM="no" \
 PROJECT_USES_WEBPACK="no" \
 PROJECT_USES_JERBIL="no" \
 PROJECT_USES_WWAPPBASE_SYMLINK="no"
-EOF
