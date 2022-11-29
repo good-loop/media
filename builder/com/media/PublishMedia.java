0e127402cab04032d138a350738e6632c97187ed
Daniel Winterstein <daniel@good-loop.com>
2022-11-29 18:00:16 +0000

    sync

--- builder/com/media/PublishMedia.java
+++ builder/com/media/PublishMedia.java
@@ -4,7 +4,6 @@ package com.media;
 import java.util.List;
 
 import com.winterwell.bob.BuildTask;
-import com.winterwell.datalog.PublishDataServer;
 import com.winterwell.web.app.build.KPubType;
 import com.winterwell.web.app.build.PublishProjectTask;
 
